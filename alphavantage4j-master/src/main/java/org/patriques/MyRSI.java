f2c90b592d8fbd7c2b0dd3e1156cbeef5926aa43
Gopal Darak <gopaldarak@gmail.com>
2019-09-30 21:21:58 -0400

    removed extra try catch blocks

--- alphavantage4j-master/src/main/java/org/patriques/MyRSI.java
+++ alphavantage4j-master/src/main/java/org/patriques/MyRSI.java
@@ -32,19 +32,20 @@ public class MyRSI {
 	private static final int DENOMINATOR = 1;
 	private static final int timeout = 50000;
 	private static final String apiKey = "J716XINOZ7UCVCTG";
-	private static AlphaVantageConnector apiConnector = new AlphaVantageConnector(apiKey,
-			timeout);
+	private static AlphaVantageConnector apiConnector = new AlphaVantageConnector(
+			apiKey, timeout);
 	private static TechnicalIndicators technicalIndicators = new TechnicalIndicators(
 			apiConnector);
 	private static TimeSeries timeSeries = new TimeSeries(apiConnector);
 	private static List<RSIData> spxInfo;
+
 	public static void main(String[] args) throws Exception {
-		
+
 		spxInfo = getSPXInfo();
 		Connection conn = DBUtils.getConnection();
 		ArrayList<String> selectStocks = DBUtils.selectStocks(conn);
-//		 ArrayList<String> selectStocks = new ArrayList<>();
-//		selectStocks.add("CCL");
+		// ArrayList<String> selectStocks = new ArrayList<>();
+		// selectStocks.add("CCL");
 
 		System.out.println("# of stocks selected :" + selectStocks.size());
 
@@ -52,29 +53,26 @@ public class MyRSI {
 			String myStock = stock.trim();
 			try {
 				List<RSIPrice> rsiPriceList = new ArrayList<RSIPrice>();
-				try {
+				// try {
+				List<RSIData> rsiList = collectRsiData(myStock);
+				List<StockData> priceList = collectPriceData(myStock);
+				List<STOCHDataSlow> stochSlowList = collectSTOCHData(myStock);
 
-					List<RSIData> rsiList = collectRsiData(myStock);
-					List<StockData> priceList = collectPriceData(myStock);
-					List<STOCHDataSlow> stochSlowList = collectSTOCHData(myStock);
+				int size = priceList.size() > rsiList.size() ? rsiList.size()
+						: priceList.size();
+				for (int i = 0; i < size; i++) {
+					StockData price = priceList.get(i);
+					RSIData rsi = rsiList.get(i);
+					STOCHDataSlow stochDataSlow = stochSlowList.get(i);
 
-					int size = priceList.size() > rsiList.size() ? rsiList
-							.size() : priceList.size();
-						for (int i = 0; i < size; i++) {
-							StockData price = priceList.get(i);
-							RSIData rsi = rsiList.get(i);
-							STOCHDataSlow stochDataSlow = stochSlowList.get(i);
-							
-							rsiPriceList.add(new RSIPrice(myStock, price
-									.getDateTime(), rsi.getValue(),
-									stochDataSlow.getSlowK(),
-									stochDataSlow.getSlowD(),
-									price.getClose()));
-						}
-					runAnalysis(myStock, rsiPriceList);
-				} catch (Exception e) {
-					System.out.println("Exception occured for " + myStock);
+					rsiPriceList.add(new RSIPrice(myStock, price.getDateTime(),
+							rsi.getValue(), stochDataSlow.getSlowK(),
+							stochDataSlow.getSlowD(), price.getClose()));
 				}
+				runAnalysis(myStock, rsiPriceList);
+				// } catch (Exception e) {
+				// System.out.println("Exception occured for " + myStock);
+				// }
 				Thread.sleep(SLEEP_INTERVAL);
 
 			} catch (AlphaVantageException e) {
@@ -87,64 +85,75 @@ public class MyRSI {
 		}
 
 	}
-//Todo-get oldest data for +/- 5 days to analyze k,d and rsi values
+
+	// Todo-get oldest data for +/- 5 days to analyze k,d and rsi values
 	private static void runAnalysis(String myStock, List<RSIPrice> rsiPriceList) {
 
-		System.out.println("Run analysis is called for " + myStock + " with size of list"+rsiPriceList.size());		
+		System.out.println("Run analysis is called for " + myStock
+				+ " with size of list" + rsiPriceList.size());
 		RSIPrice latest = rsiPriceList.stream().findFirst().get();
 		rsiPriceList
-			.stream()
-			.filter( data -> findDateinSPX(data))
-//			.filter(x -> x.getRsi() >= 35 && x.getRsi() <=45 
-//			         && ((x.getSlowK() >=15 && x.getSlowK() <=40) 
-//			              || (x.getSlowD() >=15 && x.getSlowD() <=40)
-//			            ) 
-//			        )
-			.map( y -> gain.apply(latest, y))
-			.forEach( x -> System.out.print(""));
+				.stream()
+				.filter(data -> findDateinSPX(data))
+				// .filter(x -> x.getRsi() >= 35 && x.getRsi() <=45
+				// && ((x.getSlowK() >=15 && x.getSlowK() <=40)
+				// || (x.getSlowD() >=15 && x.getSlowD() <=40)
+				// )
+				// )
+				.map(y -> gain.apply(latest, y))
+				.forEach(x -> System.out.print(""));
 	}
 
-
 	private static Boolean findDateinSPX(RSIPrice data) {
-	return spxInfo
-	.stream()
-	.filter( x -> x.getDateTime().toString().substring(0,10).equals(data.getPriceDateTime().toString().substring(0, 10)))
-	.count() > 0;
-}
-	/**
-	 * @param timeSeries
-	 * @param myStock
-	 * @return
-	 * @throws InterruptedException
-	 */
+		return spxInfo
+				.stream()
+				.filter(x -> x
+						.getDateTime()
+						.toString()
+						.substring(0, 10)
+						.equals(data.getPriceDateTime().toString()
+								.substring(0, 10))).count() > 0;
+	}
+
+	private static List<RSIData> collectRsiData(String myStock)
+			throws Exception {
+		return rsiResult(technicalIndicators, myStock).stream().collect(
+				Collectors.toList());
+	}
+
+	private static List<StockData> collectPriceData(String myStock)
+			throws Exception {
+		return dailyStockData(timeSeries, myStock).stream().collect(
+				Collectors.toList());
+	}
+
+	private static List<STOCHDataSlow> collectSTOCHData(String myStock)
+			throws Exception {
+		return stochResult(technicalIndicators, myStock).stream().collect(
+				Collectors.toList());
+	}
 
 	private static List<RSIData> rsiResult(
 			TechnicalIndicators technicalIndicators, String myStock)
 			throws InterruptedException {
-		Thread.sleep(SLEEP_INTERVAL/ DENOMINATOR);
-	    List<RSIData> rsiData = new ArrayList<RSIData>();
-		try {
-			RSI response = technicalIndicators.rsi(myStock, Interval.DAILY,
-					TimePeriod.of(15), SeriesType.CLOSE);
-			rsiData = response
-					.getData()
-					.stream()
-					.filter(x -> x.getDateTime().isAfter(
-							LocalDateTime.parse(MyRSI.START_DATE))
-							&& x.getDateTime().isBefore(LocalDateTime.now()))
-					.collect(Collectors.toList());
-		} catch (Exception e) {
-			System.out.println("Exception occured for " + myStock + ":"
-					+ e.getMessage());
-		}
-		return rsiData;
+		Thread.sleep(SLEEP_INTERVAL / DENOMINATOR);
+		RSI response = technicalIndicators.rsi(myStock, Interval.DAILY,
+				TimePeriod.of(15), SeriesType.CLOSE);
+		return response
+				.getData()
+				.stream()
+				.filter(x -> x.getDateTime().isAfter(
+						LocalDateTime.parse(MyRSI.START_DATE))
+						&& x.getDateTime().isBefore(LocalDateTime.now()))
+				.collect(Collectors.toList());
 
 	}
 
 	private static List<StockData> dailyStockData(TimeSeries timeSeries,
 			String myStock) throws InterruptedException {
-		Thread.sleep(SLEEP_INTERVAL/ DENOMINATOR);
-		DailyAdjusted dailyAdjusted = timeSeries.dailyAdjusted(myStock,OutputSize.FULL);
+		Thread.sleep(SLEEP_INTERVAL / DENOMINATOR);
+		DailyAdjusted dailyAdjusted = timeSeries.dailyAdjusted(myStock,
+				OutputSize.FULL);
 		return dailyAdjusted
 				.getStockData()
 				.stream()
@@ -153,66 +162,40 @@ public class MyRSI {
 						&& x.getDateTime().isBefore(LocalDateTime.now()))
 				.collect(Collectors.toList());
 	}
-	
-	
+
 	private static List<STOCHDataSlow> stochResult(
 			TechnicalIndicators technicalIndicators, String myStock)
 			throws InterruptedException {
-		Thread.sleep(SLEEP_INTERVAL/ DENOMINATOR);
-		List<STOCHDataSlow> stochData = new ArrayList<>();
-		try {
-			STOCH stockDataSlowResponse = technicalIndicators.stoch(myStock.trim(),Interval.DAILY , FastKPeriod.of(14), SlowKPeriod.of(3), SlowDPeriod.of(3), SlowKMaType.SMA, SlowDMaType.SMA);
-			stochData  = stockDataSlowResponse
-					.getData()
-					.stream()
-					.filter(x -> x.getDateTime().isAfter(
-							LocalDateTime.parse(MyRSI.START_DATE))
-							&& x.getDateTime().isBefore(LocalDateTime.now()))
-					.collect(Collectors.toList());
-		} catch (Exception e) {
-			System.out.println("Exception occured for " + myStock + ":"
-					+ e.getMessage());
-		}
-		return stochData;
+		Thread.sleep(SLEEP_INTERVAL / DENOMINATOR);
+		STOCH stockDataSlowResponse = technicalIndicators.stoch(myStock.trim(),
+				Interval.DAILY, FastKPeriod.of(14), SlowKPeriod.of(3),
+				SlowDPeriod.of(3), SlowKMaType.SMA, SlowDMaType.SMA);
+		return stockDataSlowResponse
+				.getData()
+				.stream()
+				.filter(x -> x.getDateTime().isAfter(
+						LocalDateTime.parse(MyRSI.START_DATE))
+						&& x.getDateTime().isBefore(LocalDateTime.now()))
+				.collect(Collectors.toList());
 
 	}
-	
-	private static List<RSIData> collectRsiData(String myStock) throws Exception { 
-	return rsiResult(technicalIndicators,
-			myStock).stream().collect(Collectors.toList());
-	}
-	
-	private static List<StockData> collectPriceData(String myStock) throws Exception {
-	return dailyStockData(timeSeries,
-			myStock).stream().collect(Collectors.toList());
-	}
-	
-	private static List<STOCHDataSlow> collectSTOCHData(String myStock) throws Exception {
-	return stochResult(technicalIndicators,
-			myStock).stream().collect(Collectors.toList());
-	}
 
-	private static BiFunction<RSIPrice, RSIPrice, Double> gain = (current,oldest) -> 
-	{
-	double roi = ((current.getPrice() - oldest.getPrice()) / oldest
-			.getPrice()) * 100;
-	System.out.println(current.getStock() + ":" + roi
-			+ "%. Current RSI:" + current.getRsi()
-            + ":date:"+ oldest.getPriceDateTime().toString().substring(0,10)				
-			+ ":Older rsi:" + oldest.getRsi()
-			+ ":Older k:"+ oldest.getSlowK()
-			+ ":Older d:"+ oldest.getSlowD()
-			);
-	return roi;
+	private static BiFunction<RSIPrice, RSIPrice, Double> gain = (current,
+			oldest) -> {
+		double roi = ((current.getPrice() - oldest.getPrice()) / oldest
+				.getPrice()) * 100;
+		System.out.println(current.getStock() + ":" + roi + "%. Current RSI:"
+				+ current.getRsi() + ":date:"
+				+ oldest.getPriceDateTime().toString().substring(0, 10)
+				+ ":Older rsi:" + oldest.getRsi() + ":Older k:"
+				+ oldest.getSlowK() + ":Older d:" + oldest.getSlowD());
+		return roi;
 	};
 
-	private static List<RSIData> getSPXInfo()throws Exception{
-		return collectRsiData("SPX")
-		.stream()
-		.filter( x -> x.getValue() < 30)
-		.collect(Collectors.toList());
+	private static List<RSIData> getSPXInfo() throws Exception {
+		return collectRsiData("SPX").stream().filter(x -> x.getValue() < 30)
+				.collect(Collectors.toList());
 
 	}
-
 
 }
